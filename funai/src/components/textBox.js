042536a95842fda2838da2f60e7ecf2a501dcaf9
JoJo-designs <jodiladouceur.design@gmail.com>
2022-05-19 09:05:31 -0400

    removing uneeded files. adding starter app Files

---
+++ funai/src/components/textBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function textBox() {
+    return (
+        <div>
+            <h1>fun with AI</h1>
+            <textarea></textarea>
+        </div>
+    )
+}
